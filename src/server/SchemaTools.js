2635facb8454342c419fb03d3724d88fb676e9b1
Pavol Starek <pavol@starek.sk>
2014-05-26 17:01:38 +0200

    SchemaTools introduced
    First implementation of schema tools that allow group of schemas
    processing.
    
    [FINISH #71643608]

---
+++ src/server/SchemaTools.js
@@ -0,0 +1,234 @@
+'use strict';
+
+var URL = require('url');
+var path = require('path');
+var extend = require('extend');
+var util = require('util');
+
+//TODO honor JSON pointer reference
+//TODO honor JSON reference reference
+//TODO honor URI reference for URI validation
+//TODO handle recursive schemas properly
+var SchemaTools = function() {
+
+	// hashTable storing registered caches
+	var schemasCache = {};
+
+	/**
+	 * Helper function that normalizes URI to common format
+	 */
+	var normalizeURL = function(url) {
+		//TODO do path normalization
+		if (!url) {
+			throw new Error('Cannot normalize undefined url');
+		}
+
+		if (!url.hash) {
+			url.hash = '#';
+		}
+
+		if (url.pathname) {
+			url.pathname = path.normalize(url.pathname);
+		}
+		return url;
+	};
+
+	/**
+	 * Add schema to schema cache for uri resolution.
+	 * If URI does not contain fragment (#) identification, # is appended to
+	 * the end of URI.
+	 *
+	 * @param {string} uri - resolution uri
+	 * @param {string|object} schema - schema as string or json object
+	 * @param {boolean} override - true if new schema should override old one
+	 */
+	this.registerSchema = function(uri, schema, override) {
+		var schemaObj = null;
+
+		if (typeof schema === 'string') {
+			schemaObj = JSON.parse(schema);
+		} else if (typeof schema === 'object') {
+			schemaObj = schema;
+		}
+
+		if (!schemaObj) {
+			throw new Error('Failed to parse schema object');
+		}
+		
+		// if there is no uri provided, extract one from schema itself
+		var url;
+		if (uri) {
+			url = normalizeURL(URL.parse(uri));
+		} else {
+			// extract id from schema
+			if (schema.id) {
+				url = normalizeURL(URL.parse(schema.id));
+			} else {
+				throw new Error('Neither uri or schema.id defined');
+			}
+		}
+
+		if (schemasCache[URL.format(url)] && !override) {
+			throw new Error('Schema already defined');
+		}
+		schemasCache[URL.format(url)] = {
+			url: url,
+			def: schema,
+			compiled: null
+		};
+	};
+
+	/**
+	 * Get registered schema. If there is no schema with coresponding uri,
+	 * it returns null. It does deep uri identification and traversing so it
+	 * can return subschema of larger schema registered by registerSchema method.
+	 */
+	this.getSchema = function(uri) {
+		//TODO do traversing in schema structure URI and fragment information
+		var url = URL.parse(normalizeURL(URL.parse(uri)));
+		return schemasCache[URL.format(url)];
+	};
+
+	var that = this;
+	var parseInternal = function(uri, schema, localPath) {
+		for (var prop in schema.def) {
+			switch (prop) {
+				case '$schema':
+				case 'id' :
+				case 'type' : 
+				case '$ref' : 
+					// skip schema keywords;
+					break;
+				default:
+					var propLocalPath = null;
+					var propUrl = null;
+
+					if (schema.def[prop].id) {
+						// id is defined, lets override canonical resolution
+						propUrl = URL.resolve(uri, schema.def[prop].id);
+						// make id argument absolute
+						schema.def[prop].id = propUrl;
+						propLocalPath = URL.parse(propUrl).hash;
+						propLocalPath = (propLocalPath && propLocalPath.length > 0 ? propLocalPath: "#");
+					} else {
+						propLocalPath = localPath + (localPath === "#" ? '' : '/') + prop;
+						propUrl = URL.resolve(uri, propLocalPath);
+					}
+
+					if ('object' === typeof schema.def[prop]) {
+						// dive only if it is object
+						that.registerSchema(propUrl, schema.def[prop], true);
+						parseInternal(propUrl, that.getSchema(propUrl), propLocalPath);
+					}
+			}
+		}
+	}
+
+	this.parse = function() {
+		for (var schemaUri in schemasCache) {
+			parseInternal(schemaUri, schemasCache[schemaUri], schemasCache[schemaUri].url.hash);
+		}
+	}
+
+	var that = this;
+	var compileInternal = function(obj) {
+		for (var prop in obj) {
+			if (('object' === typeof obj[prop]) && ("$ref" in obj[prop])) {
+				//TODO text if it is only property (check RFC)
+				//TODO support for local references
+				var compiled = that.getSchema(obj[prop].$ref).compiled;
+				if (compiled) {
+					obj[prop] = compiled;
+				} else {
+					// there is still work to do
+					return false;
+				}
+			} else if (prop === '$ref') {
+				//TODO check if it os only property
+				var compiled = that.getSchema(obj.$ref).compiled;
+				if (compiled) {
+					return compiled;	
+				} else {
+					// there is still work to do
+					return false;
+				}
+			} else if ('object' === typeof obj[prop]) {
+				// dive deeper, it is not $ref
+				if (!compileInternal(obj[prop])) {
+					// there is still work to do in lower level
+					return false;
+				}
+				// lower level is ok, lets continue with next porperty
+			}
+		}
+
+		// we iterated deeply whole object and it seams ok
+		return obj;
+	}
+
+	/**
+	 * Compiles all registered schemas into one uberSchema and
+	 * each schema individually.
+	 * Schema compilation means replacing of all $ref objects by instance
+	 * of full schema definitioin registered by related uri.
+	 */
+	this.compile = function() {
+		for (var schemaUrl in schemasCache) {
+			// first clear previous compilations
+			schemasCache[schemaUrl].compiled = null;
+		}
+
+		var allDone = false;
+		while(!allDone) {
+			allDone = true;
+			for (var schemaUrl in schemasCache) {
+				//TODO implement support for local references
+				var res = compileInternal(extend(true, {}, schemasCache[schemaUrl].def));
+				if (res) {
+					schemasCache[schemaUrl].compiled = res;
+				} else {
+					allDone = false;
+				}
+			}
+		}
+	}
+
+	this.createDefaultObject = function(uri) {
+		var compiledSchema = schemasCache[uri];
+
+		if (!compiledSchema) {
+			return false;
+		}
+
+		var iterateSchema = function(schemaFragment) {
+			if ('default' in schemaFragment) {
+				// there is default so lets use it
+				if ('object' === schemaFragment.default) {
+					return extend(true, {}, schemaFragment.default);
+				} else {
+					return schemaFragment.default;
+				}
+			}
+
+			var res = {};
+			if ('properties' in schemaFragment) {
+				// only dive in if there are properties defined
+				for (var prop in schemaFragment.properties) {
+					// all definitions in properties should be opbjects by RFC
+					res[prop] = iterateSchema(schemaFragment.properties[prop]);
+				}
+
+				return res;
+			}
+
+			return null;
+
+		};
+
+		return iterateSchema(compiledSchema.compiled);
+	}
+};
+
+module.exports = {
+	SchemaTools: SchemaTools
+}
---
+++ tests/unit/server/SchemaTools.js
@@ -0,0 +1,301 @@
+var expect = require('chai').expect;
+
+describe('SchemaTools', function() {
+	var SchemaToolsModule = require(process.cwd() + '/build/server/SchemaTools.js');
+
+	it('Simple schema registration and retriaval', function(done) {
+		var simpleSchema1 = {
+			"id": "uri://registries/simpleSchema1#"
+		};
+		var schemaTools = new SchemaToolsModule.SchemaTools();
+
+		schemaTools.registerSchema('testURI', simpleSchema1);
+		var schema = schemaTools.getSchema('testURI');
+		expect(schema.def).to.be.eql(simpleSchema1);
+
+		// schema should be registered with normalized uri
+		expect(schemaTools.getSchema('testURI#').def).to.be.eql(simpleSchema1);
+
+		// uri path should be normalized to evalueate .. and .
+		schemaTools.registerSchema('http://registries/schema1/../../schema1', simpleSchema1);
+		expect(schemaTools.getSchema('http://registries/schema1').def).to.be.eql(simpleSchema1);
+
+		// if there is no uri provided, get id from schema
+		schemaTools.registerSchema(null, simpleSchema1);
+		expect(schemaTools.getSchema('uri://registries/simpleSchema1').def).to.be.eql(simpleSchema1);
+
+		done();
+	});
+
+	it('Base schema parsing', function(done) {
+		var simpleSchema1 = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/simpleSchema1#",
+			"user": {
+				"type": "object",
+				"properties": {
+					"name": {
+						"type": "string"
+					}
+				}
+			}
+		};
+		var simpleSchema2 = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/simpleSchema2#",
+			"user2": {
+				"type": "objeect",
+				"properties": {
+					"name": {
+						"id": "uri://registries/user2#name",
+						"type": "string"
+					},
+					"lastName": {
+						"id": "user2#lastName",
+						"type": "string"
+					},
+					"address": {
+						"id": "address",
+						"type": "object",
+						"properties": {
+							"street": {
+								"type": "object"
+							}
+						}
+					}
+				}
+			}
+		};
+		var schemaTools = new SchemaToolsModule.SchemaTools();
+
+		schemaTools.registerSchema(null, simpleSchema1);
+		schemaTools.registerSchema(null, simpleSchema2);
+
+		schemaTools.parse();
+
+		expect(schemaTools.getSchema('uri://registries/simpleSchema1').def).to.be.eql(simpleSchema1);
+		expect(schemaTools.getSchema('uri://registries/simpleSchema1#user').def).to.be.eql(simpleSchema1.user);
+		expect(schemaTools.getSchema(
+			'uri://registries/simpleSchema1#user/properties/name').def
+		).to.be.eql(simpleSchema1.user.properties.name);
+
+		expect(schemaTools.getSchema('uri://registries/simpleSchema2#').def).to.be.eql(simpleSchema2);
+		expect(schemaTools.getSchema('uri://registries/simpleSchema2#user2').def).to.be.eql(simpleSchema2.user2);
+		expect(schemaTools.getSchema('uri://registries/user2#name').def).to.be.eql(simpleSchema2.user2.properties.name);
+		expect(schemaTools.getSchema(
+			'uri://registries/user2#lastName').def
+		).to.be.eql(simpleSchema2.user2.properties.lastName);
+		expect(schemaTools.getSchema(
+			'uri://registries/address').def
+		).to.deep.equal(simpleSchema2.user2.properties.address);
+		expect(schemaTools.getSchema(
+			'uri://registries/address#properties/street').def
+		).to.be.eql(simpleSchema2.user2.properties.address.properties.street);
+
+
+		done();
+	});
+
+	it('Base schema compilation', function(done) {
+		var schema1 = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/common#address",
+			"type": "object",
+			"properties": {
+				"street": {
+					"type": "string"
+				},
+				"houseNo": {
+					"type": "string"
+				},
+				"city": {
+					"type": "string"
+				},
+				"zipCode": {
+					"type": "string"
+				},
+				"country": {
+					"type": "string"
+				}
+			}
+		};
+
+		var schema2 = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/common#user",
+			"type": "object",
+			"properties": {
+				"firstName": {
+					"type": "string"
+				},
+				"lastName": {
+					"type": "string"
+				},
+				"address": {
+					"$ref": "uri://registries/common#address"
+				}
+			}
+		};
+
+		var schemaTools = new SchemaToolsModule.SchemaTools();
+		schemaTools.registerSchema(null, schema1);
+		schemaTools.registerSchema(null, schema2);
+
+		schemaTools.parse();
+		schemaTools.compile();
+
+		compSchema = schemaTools.getSchema('uri://registries/common#user').compiled;
+		expect(compSchema).to.have.deep.property('properties.address');
+		expect(compSchema).to.have.deep.property('properties.address.properties');
+		expect(compSchema).to.have.deep.property('properties.address.properties.street');
+		expect(compSchema).to.have.deep.property('properties.address.properties.street.type', 'string');
+		expect(compSchema).to.have.deep.property('properties.address.properties.city');
+		expect(compSchema).to.have.deep.property('properties.address.properties.city.type', 'string');
+
+		done();
+	});
+
+	it('Simple default object extraction', function(done) {
+		var securityCredentialsSchema = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/common#securityCredentials",
+			"type": "object",
+			"properties": {
+				"permissions": {
+					"type": "object",
+					"properties": {
+						"System User": {
+							"title": "Systémový používateľ",
+							"description": "Oprávnenie pre interagovanie so systémom",
+							"type": "boolean",
+							"default": true
+						},
+						"Registry - read": {
+							"title": "Čítane registrov",
+							"description": "Oprávnenie pre čítanie dát v registroch",
+							"type": "boolean",
+							"default": false
+						},
+						"Registry - write": {
+							"title": "Zápis do registrov",
+							"description": "Oprávnenie zápis do registrov",
+							"type": "boolean",
+							"default": true
+						}
+					}
+				}
+			}
+		};
+
+		var schemaTools = new SchemaToolsModule.SchemaTools();
+		schemaTools.registerSchema(null, securityCredentialsSchema);
+
+
+		schemaTools.parse();
+		schemaTools.compile();
+
+		defObj = schemaTools.createDefaultObject("uri://registries/common#securityCredentials");
+
+		expect(defObj).to.have.deep.property('permissions');
+		expect(defObj.permissions).to.have.deep.property('System User', true);
+		expect(defObj.permissions).to.have.deep.property('Registry - read', false);
+		expect(defObj.permissions).to.have.deep.property('Registry - write', true);
+
+		done();
+	});
+
+	it('Complex schemas test', function(done) {
+		var commonSchema = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/common#",
+			"address": {
+				"type": "object",
+				"properties": {
+					"street": {
+						"type": "string"
+					},
+					"houseNo": {
+						"type": "string"
+					},
+					"city": {
+						"type": "string"
+					},
+					"zipCode": {
+						"type": "string"
+					},
+					"country": {
+						"type": "string"
+					}
+				}
+			},
+			"user": {
+				"type": "object",
+				"properties": {
+					"firstName": {
+						"type": "string"
+					},
+					"lastName": {
+						"type": "string"
+					},
+					"securityCredentials": {
+						"type": "object",
+						"properties": {
+							"permissions": {
+								"$ref": "uri://registries/common#permissions"
+							}
+						}
+					}
+				}
+			},
+			"permissions": {
+				"type": "object",
+				"properties": {
+					"System User": {
+						"title": "Systémový používateľ",
+						"description": "Oprávnenie pre interagovanie so systémom",
+						"type": "boolean",
+						"default": true
+					},
+					"Registry - read": {
+						"title": "Čítane registrov",
+						"description": "Oprávnenie pre čítanie dát v registroch",
+						"type": "boolean",
+						"default": false
+					},
+					"Registry - write": {
+						"title": "Zápis do registrov",
+						"description": "Oprávnenie zápis do registrov",
+						"type": "boolean",
+						"default": true
+					}
+				}
+			}
+		};
+
+		var redefSchema = {
+			"$schema": "http://json-schema.org/schema#",
+			"id": "uri://registries/commonRedef#",
+			"address": {"$ref": "uri://registries/common#address"},
+			"user": {"$ref": "uri://registries/common#user"},
+			"permissions": {"$ref": "uri://registries/common#permissions"}
+		};
+
+
+		var schemaTools = new SchemaToolsModule.SchemaTools();
+		schemaTools.registerSchema(null, commonSchema);
+		schemaTools.registerSchema(null, redefSchema);
+
+		schemaTools.parse();
+		schemaTools.compile();
+
+		var defObj = schemaTools.createDefaultObject("uri://registries/commonRedef#user");
+
+		console.log(defObj);
+
+		expect(defObj).to.have.deep.property('securityCredentials');
+		expect(defObj).to.have.deep.property('securityCredentials.permissions');
+		expect(defObj.securityCredentials.permissions).to.have.deep.property('System User', true);
+
+		done();
+	});
+});

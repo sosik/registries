540e7c5ef11c173357f0d12c775edb76a4b534dc
petugez <petugez@gmail.com>
2014-06-04 15:53:57 +0200

    Vylistovanie Roly [Finished #71381946]

---
+++ tests/unit/server/securityCtrlTest.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect;
+var util = require('util');
+var securityModule = require(process.cwd() + '/build/server/securityController.js');
+
+
+describe('SecurityCtrl', function() {
+	beforeEach(function(done) {
+		done();
+	});
+
+	afterEach(function(done) {
+		done();
+	});
+
+	var securityCtrl = new securityModule.SecurityController();
+
+	it('getRoles should return non empty list of roles', function(done) {
+		
+		var reqMock = {};
+		
+		var resMock = function() {
+			require('stream').Writable.call(this);
+			this.statusCode = null;
+			this.data = '';
+			this.send = function(code, data) {
+				console.log(data);
+				expect(data.length).to.be.above(1);
+				done();
+			};
+		};
+		util.inherits(resMock, require('stream').Writable);
+		
+		securityCtrl.getRoles(reqMock,res= new resMock());
+		
+	});
+
+	
+	
+});

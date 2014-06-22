d2fcb2ad4c5bc60cfe7bcd70271c4342e82d64f1
Pavol Starek <pavol@starek.sk>
2014-06-22 23:19:42 +0200

    [#72327972] Localization support in code and schema
    - example implementation of localization of static text
    - example implementation of localization of schema fields
    
    [FINISHED #72327972]

--- src/client/js/registry.js
+++ src/client/js/registry.js
@@ -365,17 +365,17 @@ angular.module('registry', ['schema-utils'])
 						fieldSet.append('<label>'+value.title+'</label>');
 						angular.forEach(value.properties, function(value2, key2) {
 							if (value2.render && value2.render.component === 'psui-datepicker') {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<input psui-validity-mark psui-datepicker required type="text" class="form-control" placeholder="" ng-model="'+options.modelPath+'.'+key+'.'+key2+'"/></div></div></div>');
 							} else if (value2.render && value2.render.component === 'psui-uploadable-image') {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<psui-uploadable-image psui-imageresizor psui-imageresizor-width="' +value2.render.width
 								+ '" psui-imageresizor-height="'+value2.render.height
 								+'" ng-model="'+options.modelPath+'.'+key+'.'+key2+'" style="'+(value2.render.width ? 'width:'+value2.render.width+'px !important;':'')+(value2.render.height ? 'height:'+value2.render.height+'px !important;':'')+'"/></psui-uploadable-image></div></div>');
 							} else {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<input psui-validity-mark required type="text" class="form-control" placeholder="" ng-model="'+options.modelPath+'.'+key+'.'+key2+'"/></div></div></div>');
 							}
@@ -416,15 +416,15 @@ angular.module('registry', ['schema-utils'])
 						fieldSet.append('<label>'+value.title+'</label>');
 						angular.forEach(value.properties, function(value2, key2) {
 							if (value2.render && value2.render.component === 'psui-datepicker') {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<input psui-validity-mark psui-datepicker psui-inlineedit="view" required type="text" class="form-control" placeholder="" ng-model="'+options.modelPath+'.'+key+'.'+key2+'"/></div></div></div>');
 							} else if (value2.render && value2.render.component === 'psui-uploadable-image') {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<psui-uploadable-image ng-model="'+options.modelPath+'.'+key+'.'+key2+'" style="'+(value2.render.width ? 'width:'+value2.render.width+'px !important;':'')+(value2.render.height ? 'height:'+value2.render.height+'px !important;':'')+'"/></psui-uploadable-image></div></div>');
 							} else {
-								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+value2.title+'</label>'
+								fieldSet.append('<div class="form-group"><label class="col-sm-4 control-label">'+(value2.transCode ? '{{\''+ value2.transCode+'\'| translate}}' : value2.title)+'</label>'
 								+'<div class="col-sm-8"><div class="input-group">'
 								+'<input psui-validity-mark psui-inlineedit="view" required type="text" class="form-control" placeholder="" ng-model="'+options.modelPath+'.'+key+'.'+key2+'"/></div></div></div>');
 							}
---
+++ src/client/js/translations.js
@@ -0,0 +1,35 @@
+angular.module('registries')
+.config(['$translateProvider', function($translateProvider) {
+	$translateProvider.preferredLanguage('sk');
+
+	$translateProvider.translations('sk', {
+		'login.title': 'Prihlásenie',
+		'login.loginName': 'Prihlasovacie meno',
+		'login.password': 'Heslo',
+		'login.forgottenPassword': 'Zabudnuté heslo',
+		"schema.people.baseData": "Základné informácie",
+		"schema.people.baseData.identifier": "Identifikátor",
+		"schema.people.baseData.name": "Meno",
+		"schema.people.baseData.surName": "Priezvisko",
+		"schema.people.baseData.bornName": "Rodné priezvysko",
+
+
+	});
+
+	$translateProvider.translations('cz', {
+		'login.title': 'Přihlášení',
+		'login.loginName': 'Přihlašovací jméno',
+		'login.password': 'Heslo',
+		'login.forgottenPassword': 'Zapomenuté heslo',
+		"schema.people.baseData": "Základní informace",
+		"schema.people.baseData.identifier": "Identifikátor",
+		"schema.people.baseData.name": "Jméno",
+		"schema.people.baseData.surName": "Příjmení",
+		"schema.people.baseData.bornName": "Rodné příjmení",
+	});
+}])
+.controller('langSelectCtrl', ['$scope', '$translate', function($scope, $translate) {
+	$scope.setLang = function(lang) {
+		$translate.use(lang);
+	};
+}]);

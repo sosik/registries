d08377226dddd2de583e056056ad91d357ab2578
Pavol Starek <pavol@starek.sk>
2014-07-03 12:11:29 +0200

    Tool for managed setup of data directory
    - tool for registration and management of particular dataset cloning

---
+++ tools/setupData.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+declare -A datasets
+
+datasets=(
+	["caihp"]="https://github.com/sosik/registries-data-caihp.git"
+)
+
+if [ $# -lt 1 ]; then
+	echo "---------------------------------------------"
+	echo "Creates data directory for particular dataset"
+	echo
+	echo "Usage:"
+	echo "$0 <dataset>"
+	echo
+	echo "Known datasets:"
+	for set in ${!datasets[@]}; do echo "$set => ${datasets["$set"]}"; done;
+	echo
+
+	exit -1;
+fi
+
+# do stuff
+done=0;
+for set in ${!datasets[@]}; do
+	if [ "$1" == "$set" ]; then
+		done=1;
+		echo "Requested set $set"
+		echo "Cloning ${datasets["$set"]}"
+
+		git clone ${datasets[$set]} data
+		exit 0;
+	fi
+done;
+
+if [ $done -eq 0 ]; then
+	echo "Unrecognized dataset $1"
+	exit -1;
+fi;

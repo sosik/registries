429e0719ee4a601e64af4ac0e8422a26822f0482
Pavol Starek <pavol@starek.sk>
2014-07-03 13:19:55 +0200

    Support for restriction of allowed fs operations over REST Api
    - fsController now allow configuration of allowed operations

--- src/server/fsController.js
+++ src/server/fsController.js
@@ -10,92 +10,103 @@ var app = express();
 
 var DEFAULT_CFG = {
 	rootPath : '/tmp',
-	filePathRegexp : /^\/fs\/\w+/
+	filePathRegexp : /^\/fs\/\w+/,
+	allowedOperations: [ 'ls', 'get', 'rm', 'mkdir', 'put', 'putgetpath', 'replace' ]
 };
 
 app.cfg = function(options) {
 	var prop = extend(true, {}, DEFAULT_CFG, options);
 	app.prop = prop;
 	app.fsCtrl = new fsCtrlModule.FsCtrl(prop);
+
+// Register only allowed operations
+// ls
+	if (prop.allowedOperations.indexOf('ls') > -1) {
+		app.get('/ls*', function(req, resp) {
+			var path = req.url.substring(3);
+
+			app.fsCtrl.ls(path, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+// get
+	} else if (prop.allowedOperations.indexOf('get') > -1) {
+		app.get('/get/*', function(req, resp) {
+			var path = req.url.substring(5);
+
+			app.fsCtrl.get(path, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+// rm
+	} else if (prop.allowedOperations.indexOf('rm') > -1) {
+		app.get('/rm/*', function(req, resp) {
+			var path = req.url.substring(4);
+
+			app.fsCtrl.rm(path, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+// mkdir
+	} else if (prop.allowedOperations.indexOf('mkdir') > -1) {
+		app.get('/mkdir/*', function(req, resp) {
+			var path = req.url.substring(7);
+
+			app.fsCtrl.mkdir(path, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+// put
+	} else if (prop.allowedOperations.indexOf('put') > -1) {
+		app.put('/put/*', function(req, resp) {
+			var path = req.url.substring(5);
+
+			app.fsCtrl.put(path, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+// putgetpath
+	} else if (prop.allowedOperations.indexOf('putgetpath') > -1) {
+		app.put('/putgetpath/*', function(req, resp) {
+			var path = req.url.substring('/putgetpath/'.length);
+
+			app.fsCtrl.putGetPath(path, req, req.get('Content-Type'), function(err, path) {
+				if (err) {
+					log.error('Failed to putGetPath', err);
+					resp.send(500, err);
+				}
+				log.info('Saved file %s', path);
+				resp.send(200, path);
+			});
+		});
+// replace
+	} else if (prop.allowedOperations.indexOf('replace') > -1) {
+		app.put('/replace/*', function(req, resp) {
+			var path = req.url.substring(9);
+
+			app.fsCtrl.replace(path, req, resp, function(err) {
+				if (err) {
+					resp.send(err.code || 500, err);
+				}
+
+			});
+		});
+	}
 };
-
-app.get('/ls*', function(req, resp) {
-	var path = req.url.substring(3, req.url.lenght);
-
-	app.fsCtrl.ls(path, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
-
-app.get('/get/*', function(req, resp) {
-	var path = req.url.substring(5, req.url.lenght);
-
-	app.fsCtrl.get(path, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
-
-app.get('/rm/*', function(req, resp) {
-	var path = req.url.substring(4, req.url.lenght);
-
-	app.fsCtrl.rm(path, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
-
-app.get('/mkdir/*', function(req, resp) {
-	var path = req.url.substring(7, req.url.lenght);
-
-	app.fsCtrl.mkdir(path, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
-
-app.put('/put/*', function(req, resp) {
-	var path = req.url.substring(5, req.url.lenght);
-
-	app.fsCtrl.put(path, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
-
-app.put('/putgetpath/*', function(req, resp) {
-	var path = req.url.substring('/putgetpath/'.length, req.url.lenght);
-
-	app.fsCtrl.putGetPath(path, req, req.get('Content-Type'), function(err, path) {
-		if (err) {
-			log.error('Failed to putGetPath', err);
-			resp.send(500, err);
-		}
-		log.info('Saved file %s', path);
-		resp.send(200, path);
-	});
-});
-
-app.put('/replace/*', function(req, resp) {
-	var path = req.url.substring(9, req.url.lenght);
-
-	app.fsCtrl.replace(path, req, resp, function(err) {
-		if (err != null) {
-			resp.send(err.code || 500, err);
-		}
-
-	});
-});
 
 module.exports = app;

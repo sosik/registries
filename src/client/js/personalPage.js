42c6dd2ef399d5f3e6c78270be92399d7ed1751c
Pavol Starek <pavol@starek.sk>
2014-07-08 11:50:08 +0200

    Dirty solution for personal page redirection
    - personal page now redirects to own person data view

--- src/client/js/personalPage.js
+++ src/client/js/personalPage.js
@@ -1,3 +1,5 @@
 angular.module('personal-page', [])
-.controller('personalPageCtrl', [function() {
+.controller('personalPageCtrl', ["$scope", "$location", function($scope, $location) {
+	console.log('/registry/view/uri~3A~2F~2Fregistries~2Fmember~23/'+$scope.security.currentUser.id);
+	$location.path('/registry/view/uri~3A~2F~2Fregistries~2Fmember~23/'+$scope.security.currentUser.id);
 }]);

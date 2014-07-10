8fd7d5cc8e80c6079afb8bf774406e49714d50c3
Pavol Starek <pavol@starek.sk>
2014-07-10 14:57:30 +0200

    psui-default-src shwod default image on empty ng-src
    - shows default value when attribute ng-src is empty

--- src/client/js/psui-default-src.js
+++ src/client/js/psui-default-src.js
@@ -8,12 +8,17 @@ angular.module('psui-default-src', [])
 			if (attrs.ngSrc) {
 				// ngSrc is set so lets watch its expression and set default src if ngSrc value is empty
 				// TODO handle attribute change by $observe
-				scope.$watch(attrs.ngSrc, function(newVal) {
+				scope.$watch(function() {
+					return attrs.ngSrc;
+				}, function(newVal) {
+					console.log(newVal + attrs.ngSrc);
 					if (newVal === null || newVal.length < 1) {
 						// src is not set, lets use default src
 						elm.attr('src', attrs.psuiDefaultSrc);
 					}
 				});
+			} else {
+				elm.attr('src', attrs.psuiDefaultSrc);
 			}
 
 			// handle 404 and other error states by default inmage

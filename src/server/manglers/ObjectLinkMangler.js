cc6cc0c468db6d4b5fd035f9de67a77a71e5ed9c
Pavol Starek <pavol@starek.sk>
2014-08-25 01:12:37 +0200

    ObjectLink mangler and unmangler
    - ObjectLink mangler
    - ObjectLink unmangler

---
+++ src/server/manglers/ObjectLinkMangler.js
@@ -0,0 +1,37 @@
+(function() {
+	'use strict';
+
+	var consts = require('./../SchemaConstants.js');
+	var log = require('./../logging.js').getLogger('manglers/ObjectLinkMangler.js');
+
+	function ObjectLinkMangler() {
+	}
+
+	ObjectLinkMangler.prototype.mangle = function(objFragment, schemaFragment, objPath, callback) {
+		log.silly('ObjectLink mangler start for %s', objPath);
+		var prop;
+
+		if (!objFragment || !schemaFragment || !schemaFragment[consts.OBJECT_LINK_KEYWORD]) {
+			log.silly('Nothing to mangle');
+			callback(null, null);
+			return;
+		}
+
+		// remove all uneeded properties
+		for (prop in objFragment) {
+			if ((prop !== consts.OBJECT_LINK_REGISTRY_KEYWORD) && (prop !== consts.OBJECT_LINK_OID_KEYWORD)) {
+				log.silly('Deleting field %s', prop);
+				delete objFragment[prop];
+			}
+		}
+
+		log.debug('ObjectLink mangling finished for %s',  objPath);
+		callback(null, []);
+	};
+
+	module.exports = function() {
+		return new ObjectLinkMangler();
+	};
+}());
+
+
---
+++ src/server/manglers/ObjectLinkUnmangler.js
@@ -0,0 +1,75 @@
+(function() {
+	'use strict';
+
+	var consts = require('./../SchemaConstants.js');
+	var objectTools = require('./../ObjectTools.js');
+	var log = require('./../logging.js').getLogger('manglers/ObjectLinkUnmangler.js');
+
+	function ObjectLinkUnmangler(daoFactory, mongoDriver) {
+		this.daoFactory = daoFactory;
+		this.mongoDriver = mongoDriver;
+	}
+
+	ObjectLinkUnmangler.prototype.mangle = function(objFragment, schemaFragment, objPath, callback) {
+		log.silly('ObjectLink unmangler start for %s', objPath);
+		var props = {};
+		var prop;
+
+		if (!schemaFragment || !schemaFragment[consts.OBJECT_LINK_KEYWORD]) {
+			log.silly('Nothing to unmangle - schema');
+			callback(null, null);
+			return;
+		}
+
+		if (!objFragment || !objFragment[consts.OBJECT_LINK_OID_KEYWORD]) {
+			log.silly('Nothing to unmangle - object');
+			callback(null, null);
+			return;
+		}
+
+		var registry = objFragment[consts.OBJECT_LINK_REGISTRY_KEYWORD] || schemaFragment[consts.OBJECT_LINK_REGISTRY_KEYWORD];
+
+		if (!registry) {
+			log.debug('Failed to identify registry');
+			callback(null, null);
+			return;
+		}
+
+
+		for (prop in schemaFragment[consts.OBJECT_LINK_KEYWORD]) {
+			if ((prop !== consts.OBJECT_LINK_REGISTRY_KEYWORD)) {
+				log.silly('Found prop %s -> %s', prop, schemaFragment[consts.OBJECT_LINK_KEYWORD][prop]);
+				props[prop] = schemaFragment[consts.OBJECT_LINK_KEYWORD][prop];
+			}
+		}
+
+		var localDao = this.daoFactory(this.mongoDriver, {collectionName: registry});
+
+		localDao.get(objFragment[consts.OBJECT_LINK_OID_KEYWORD], function(err, data) {
+			var prop;
+
+			if (err) {
+				log.error('Failed to get data from collection, objectLink resolution failed');
+				callback(err);
+				return;
+			}
+
+			if (data) {
+				objFragment[consts.OBJECT_LINK_REGISTRY_KEYWORD] = registry;
+
+				objFragment[consts.OBJECT_LINK_REFDATA_KEYWORD] = {};
+				for (prop in props) {
+					objFragment[consts.OBJECT_LINK_REFDATA_KEYWORD][prop] = objectTools.evalPath(data, props[prop]);
+				}
+			}
+			log.debug('ObjectLink unmangling finished for %s',  objPath);
+			callback(null, []);
+		});
+	};
+
+	module.exports = function(daoFactory, mongoDriver) {
+		return new ObjectLinkUnmangler(daoFactory, mongoDriver);
+	};
+}());
+
+

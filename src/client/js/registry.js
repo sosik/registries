42a1531c39b17fd4409d069edf6cc7f8b8f250c5
Pavol Starek <pavol@starek.sk>
2014-09-17 22:05:24 +0200

    DateToString filter
    - filter that converts numeric date value to string

--- src/client/js/registry.js
+++ src/client/js/registry.js
@@ -105,6 +105,23 @@ angular.module('registry', ['schema-utils', 'psui', 'psui.form-ctrl', 'psui-obje
 		
 	})
 }])
+.filter('dateToString', function() {
+	return function(value) {
+		if (value) {
+			var year = value.substring(0,4);
+			var month = value.substring(4,6);
+			var day = value.substring(6,8);
+			if (year.length === 4 && month.length === 2 && day.length === 2) {
+				var d = new Date(year, month-1, day);
+
+				return d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear();
+			}
+
+			return value;
+		}
+		return '';
+	}
+})
 .directive('svfSpecialNote', ['$http', function($http) {
 	return {
 		restrict: 'E',

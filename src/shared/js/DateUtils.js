611afba4e823e5eb7591a879d0e3823300b14c38
petugez <petugez@gmail.com>
2014-09-24 14:02:10 +0200

    Generate payments

---
+++ src/shared/js/DateUtils.js
@@ -0,0 +1,95 @@
+var DateUtils = function() {
+
+	var self=this;
+	this.reverseToStr=function(strReverseDate){
+
+		var year = strReversDate.substring(0,4);
+		var month = strReversDate.substring(4,6);
+		var day = strReversDate.substring(6,8);
+		if (year.length === 4 && month.length === 2 && day.length === 2) {
+			var d = new Date(year, month-1, day);
+			return d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear();
+		}
+		return null;
+	};
+
+	this.strAddDays=function(strDate,days){
+
+		var year = strReversDate.substring(0,4);
+		var month = strReversDate.substring(4,6);
+		var day = strReversDate.substring(6,8);
+
+		if (year.length === 4 && month.length === 2 && day.length === 2) {
+			var d = new Date(year, month-1, day);
+			d.setDate(d.getDate()+days);
+			return self.dateToStr(d);
+		}
+		return null;
+	};
+
+	this.dateAddDays=function(dateDate,days){
+
+		var d = new Date(dateDate);
+		d.setDate(dateDate.getDate()+days);
+		return (d);
+	};
+
+	this.dateToStr=function(dateDate){
+		var ys = dateDate.getFullYear().toString(10);
+		var ms = (dateDate.getMonth() + 1).toString(10);
+
+		if (ms.length < 2) {
+			ms = '0'.concat(ms);
+		}
+
+		var ds = dateDate.getDate().toString();
+		if (ds.length < 2) {
+			ds = '0'.concat(ds);
+		}
+
+		return ds.conat('.',ms,'.',ys);
+	};
+
+	this.dateToReverse=function(dateDate){
+		var ys = dateDate.getFullYear().toString(10);
+		var ms = (dateDate.getMonth() + 1).toString(10);
+
+		if (ms.length < 2) {
+			ms = '0'.concat(ms);
+		}
+
+		var ds = dateDate.getDate().toString();
+		if (ds.length < 2) {
+			ds = '0'.concat(ds);
+		}
+
+		return ys.concat(ms, ds);
+	};
+
+
+	this.strToReverse=function(strDate){
+		var d = new Date();
+		var s = value.split('.');
+		if (s.length === 3) {
+			var day = parseInt(s[0]);
+			var month = parseInt(s[1]);
+			var year = parseInt(s[2]);
+
+			if ((day > 0 && day < 32) && (month > 0 && month <13) && (year > 0 && year < 10000)) {
+				d.setYear(year);
+				d.setMonth(month -1);
+				d.setDate(day);
+				return self.dateToStr(d);
+			}
+							// invalid
+							return null;
+						}
+						// invalid
+						return null;
+
+					};
+				};
+
+				module.exports = {
+					DateUtils: new DateUtils()
+				};
---
+++ util/init/generatePayments.js
@@ -0,0 +1,157 @@
+'use strict';
+var fs = require('fs');
+
+
+var mongoDriver = require('./../../build/server/mongoDriver.js');
+var config = require('./../../build/server/config.js');
+var renderServiceModule= require('./../../build/server/renderService.js');
+
+var universalDaoModule = require('./../../build/server/UniversalDao.js');
+
+var schemaRegistryModule = require('./../../build/server/schemaRegistry.js');
+
+var universalDaoControllerModule = require('./../../build/server/UniversalDaoController.js');
+
+var dateUtils = require('./../../build/server/DateUtils.js').DateUtils;
+
+var async = require('async');
+var path= require('path');
+var uuid = require('node-uuid');
+var util= require('util');
+
+
+
+process.argv.forEach(function(val, index, array) {
+	console.log(index + ': ' + val);
+});
+
+console.log('file to process', process.argv[2]);
+
+var schema='uri://registries/member#search';
+
+mongoDriver.init(config.mongoDbURI, function(err) {
+	if (err) {
+		throw err;
+	}
+
+	var schemasListPaths = JSON.parse(
+		fs.readFileSync(path.join(config.paths.schemas, '_index.json')))
+		.map(function(item) {
+			return path.join(config.paths.schemas, item);
+	});
+
+	var schemaRegistry = new schemaRegistryModule.SchemaRegistry({schemasList:schemasListPaths});
+	var udc = new universalDaoControllerModule.UniversalDaoController(mongoDriver, schemaRegistry);
+	var renderService = new renderServiceModule.RenderService();
+
+	var feeDao = new universalDaoModule.UniversalDao(mongoDriver, {
+				collectionName :'fees'
+	});
+
+	fs.mkdir('mails',function(e){
+		if(!e || (e && e.code === 'EEXIST')){
+				//do something with contents
+			} else {
+				//debug
+				console.log(e);
+			}
+			var uid=uuid.v4()
+			go(udc,feeDao,renderService,schema,uid,function(err){if (err) console.log(err); mongoDriver.close(); console.log('import.id',uid) });
+	});
+
+});
+
+
+function go(udc,feeDao,renderService,schema,uid,callback) {
+
+	var createdOn=new Date();
+
+	var req={params:{schema:schema},body:{'criteria':[{'f':'membershipFeeInfo.paymentFrequency','v':'12xročne','op':'eq'}]}};
+
+	var res=function (){
+		this.send=function (code ,data){
+			iterateData(uid,data,udc,feeDao,renderService,createdOn,callback);
+		};
+		this.json=function(data){
+			iterateData(uid,data,udc,feeDao,renderService,createdOn,callback);
+		};
+	};
+
+	util.inherits(res, require('stream').Writable);
+	res= new res();
+
+	req['perm']={'Registry - read':true};
+
+	udc.searchBySchema(req,res);
+
+}
+
+
+function iterateData(uid,data,udc,feeDao,renderService,createdOn,cb){
+	var index=1;
+	var toCall=data.map(function (item){
+		return function(callback){
+			saveItem(uid,item,udc,feeDao,renderService,index++,createdOn,callback);
+		}
+	})
+
+	async.parallel(toCall,cb);
+}
+
+function saveItem(uid,item,udc,feeDao,renderService,index,createdOn,callback){
+
+		var bill = {"import":{id:uid}, baseData:{member:{registry:'people',oid:item.id},membershipFee:item.membershipFeeInfo.membershipFee,
+							setupDate:dateUtils.dateToReverse(createdOn),
+							dueDate:dateUtils.dateToReverse(dateUtils.dateAddDays(createdOn,15)),feePaymentStatus:'Nezaplatené',variableSymbol:createVS(dateUtils.dateToReverse(createdOn),index)}};
+
+		createMail(renderService,index,item,bill);
+		feeDao.save(bill,callback);
+
+}
+
+function createVS(createdOnReverse, index){
+	var i=''+index;
+	while (i.length<6){
+		i='0'+i;
+	}
+
+	return createdOnReverse.substring(2,6)+i;
+}
+
+
+function createMail(renderService,index,user,bill){
+	var template='From: caihp@unionsoft.sk\n'+
+	'To: {{email}}\n'+
+	'Return-Path: websupport@unionsoft.sk\n'+
+	'MIME-Version: 1.0\n'+
+	'Content-Type: multipart/alternative;\n'+
+	'boundary=\'PAA08673.1018277622/unionsoft.sk\'\n'+
+	'Subject: Test HTML e-mail.\n'+
+
+	'This is a MIME-encapsulated message\n'+
+
+	'--PAA08673.1018277622/unionsoft.sk\n'+
+	'Content-type: text/plain; charset=iso-8859-1\n'+
+	'Vážený {{name}} {{surname}},\n'+
+
+	'Dovoľte mi zaslať Vám informáciu o výške členského príspevku v českej asociácii hráčov hokeja. Mesačný príspevok činí {{fee}}.CZK.\n'+
+	'Prosím poukážte uvedenú čiastku na bankový účet asociácie č. 2110167935 / 2700. Pri uhrade  uvedte nasledovny variabilný symbol {{variableSymbol}}.\n'+
+	'Ďakujeme,\n'+
+	'--PAA08673.1018277622/unionsoft.sk\n';
+
+// '--PAA08673.1018277622/server.xyz.com\n'+
+// 'Content-Type: text/html\n'+
+// '{{htmlVersion}}\n'+
+// '--PAA08673.1018277622/server.xyz.com\n'+
+
+
+	var resolvedHtml=renderService.renderInstant(template,{locals:{'name':user.baseData.name,'surname':user.baseData.surName,email:user.contactInfo.email,'fee':bill.baseData.membershipFee,'variableSymbol':bill.baseData.variableSymbol}});
+
+	fs.writeFile('mails/mail-'+bill.baseData.variableSymbol+'-'+user.baseData.name+'.'+user.baseData.surName+'.toSend',resolvedHtml, function(err) {
+		if(err) {
+				console.log(err);
+		}
+	});
+
+
+}

e14e364427b0e3cf2f92291a47deb4bdb480577b
Pavol Starek <pavol@starek.sk>
2014-11-06 11:18:21 +0100

    Fieldset schema attribute renamed to options
    - xpsui-schema renamend to xpsui-options

--- src/client/js/xpsui/directives/fieldset.js
+++ src/client/js/xpsui/directives/fieldset.js
@@ -2,25 +2,46 @@
 	'use strict';
 
 	angular.module('xpsui:directives')
+
+	/**
+	 * Renders and manages block of form components. Mainly used for schema render.
+	 *
+	 * @module client
+	 * @submodule directives
+	 * @class xpsui-fieldset
+	 */
 	.directive('xpsuiFieldset', ['xpsui:logging', 'xpsui:FormGenerator', function(log, formGenerator) {
 		return {
 			restrict: 'A',
 			link: function(scope, elm, attrs, ctrls) {
 				log.group('FieldSet Link');
 
+				/**
+				 * NgModel binding for fieldset
+				 * @default false
+				 * @required true
+				 * @attribute xpsui-model
+				 */
 				var modelPath = attrs.xpsuiModel;
-				var schemaPath = attrs.xpsuiSchema;
+
+				/**
+				 * Options used for directive processing. It can consume schema fragment
+				 * @default false
+				 * @required true
+				 * @attribute xpsui-options
+				 */
+				var options = attrs.xpsuiOptions;
 
 				elm.addClass('x-fieldset');
 
-				if (modelPath && schemaPath) {
+				if (modelPath && options) {
 				} else {
-					log.warn('Attributes xpsui-model and xpsui-schema have to be set, skipping fieldset generation');
+					log.warn('Attributes xpsui-model and xpsui-options have to be set, skipping fieldset generation');
 					log.groupEnd();
 					return;
 				}
 
-				var schema = scope.$eval(schemaPath);
+				var schema = scope.$eval(options);
 				var mode = attrs.xpsuiFieldset;
 
 				if (schema && schema.title) {
@@ -30,7 +51,7 @@
 
 				var content = angular.element('<div class="x-fieldset-content"></div>');
 				elm.append(content);
-				formGenerator.generateForm(scope, content, schema, schemaPath, modelPath, mode || formGenerator.MODE.VIEW);
+				formGenerator.generateForm(scope, content, schema, options, modelPath, mode || formGenerator.MODE.VIEW);
 
 				log.groupEnd();
 			}

ba98d45bc63562ebd9d7ea59f87c2e3929104ac3
Martin Balaz <balaz.martin.mgr@gmail.com>
2014-12-09 01:03:30 -0800

    XPSUI objectlink2 mobile/tablet view
    - fix object link view
    - update css styles

--- src/client/js/xpsui/directives/objectlink2-edit.js
+++ src/client/js/xpsui/directives/objectlink2-edit.js
@@ -2,13 +2,15 @@
 	'use strict';
 
 	angular.module('xpsui:directives')
-	.directive('xpsuiObjectlink2Edit', ['xpsui:logging','$parse', 'xpsui:DropdownFactory', 'xpsui:Objectlink2Factory','xpsui:SelectDataFactory', function(log, $parse, dropdownFactory, selectboxFactory, datafactory) {
+	.directive('xpsuiObjectlink2Edit', ['xpsui:logging','$parse', 'xpsui:DropdownFactory', 'xpsui:Objectlink2Factory','xpsui:SelectDataFactory',
+	function(log, $parse, dropdownFactory, objectlink2Factory, dataFactory) {
 		return {
 			restrict: 'A',
 			require: ['ngModel', '?^xpsuiFormControl', 'xpsuiObjectlink2Edit'],
 			controller: function($scope, $element, $attrs) {
 				this.setup = function(){
-					this.$input = angular.element('<input></input>');
+					this.$input = angular.element('<div tabindex="0"></div>');
+					this.$input.addClass('x-input');
 				}
 				
 				this.getInput = function(){
@@ -21,18 +23,63 @@
 			link: function(scope, elm, attrs, ctrls) {
 				log.group('String edit Link');
 
-				var ngModel = ctrls[0];
-				var formControl = ctrls[1] || {};
-				var selfControl = ctrls[2];
-				var input = selfControl.getInput();
+				var ngModel = ctrls[0],
+					formControl = ctrls[1] || {},
+					selfControl = ctrls[2],
+					input = selfControl.getInput(),
+					schemaFragment = elm.data('schemaFragment')
+				;
 
 				elm.addClass('x-control');
-				elm.addClass('x-select-edit');
+				elm.addClass('x-select-edit x-objectlink2-edit');
 
 				ngModel.$render = function() {
-					input.val(ngModel.$viewValue || '');
-					formControl.oldValue = ngModel.$modelValue;
+					input.empty();
+
+					if (ngModel.$viewValue) {
+						render(ngModel.$viewValue);
+
+						// if (ngModel.$viewValue.refdata) {
+						// 	for (var i in ngModel.$viewValue.refdata) {
+						// 		var fieldSchemaFragment = dataFactory.getFieldSchemaFragment(
+						// 				schemaFragment.objectlink2.schema, schemaFragment.objectlink2.fields[i], scope
+						// 			),
+						// 			type = fieldSchemaFragment.type,
+						// 			label = fieldSchemaFragment.title,
+						// 			value = ngModel.$viewValue.refdata[i]
+						// 		;
+
+						// 		input.append(
+						// 			angular.element('<span title="' + label + '">' 
+						// 				+ objectlink2Factory.getFormatedValue(type,value) 
+						// 				+ '</span>'
+						// 			)
+						// 		);
+						// 	}
+						// }
+					}
 				};
+
+				function render(data){
+					input.empty();
+
+					for (var i in data.refdata) {
+						var fieldSchemaFragment = dataFactory.getFieldSchemaFragment(
+								schemaFragment.objectlink2.schema, schemaFragment.objectlink2.fields[i], scope
+							),
+							type = fieldSchemaFragment.type,
+							label = fieldSchemaFragment.title,
+							value = data.refdata[i]
+						;
+
+						input.append(
+							angular.element('<span title="' + label + '">' 
+								+ objectlink2Factory.getFormatedValue(type,value) 
+								+ '</span>'
+							)
+						);
+					}
+				}
 
 				elm.append(input);
 
@@ -53,9 +100,9 @@
 				;
 
 				// selectobx
-				var selectbox = selectboxFactory.create(elm, {
-					onSelected: function(index, key, value){
-						input.val(value);
+				var selectbox = objectlink2Factory.create(elm, {
+					onSelected: function(value){
+						render(value);
 						console.log('onSelected');
 						console.log(arguments);
 					}
@@ -64,7 +111,7 @@
 				selectbox.setDropdown(dropdown);
 
 				// store
-				var dataset = datafactory.createTestDataset(scope, elm.data('schemaFragment'), 6000);
+				var dataset = dataFactory.createTestDataset(scope, schemaFragment, 6000);
 				selectbox.setDataset(dataset);
 
 				log.groupEnd();

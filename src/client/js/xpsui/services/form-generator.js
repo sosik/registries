0f57e12e395111dddd607979f40f7800d00057b6
Martin Balaz <balaz.martin.mgr@gmail.com>
2015-03-24 02:19:29 -0700

    [#90674292] XPSUI form generator
    - convert all component types to lower
    
    `

--- src/client/js/xpsui/services/form-generator.js
+++ src/client/js/xpsui/services/form-generator.js
@@ -42,7 +42,7 @@
 				field.attr('xpsui-schema', schemaPath);
 			} else if (mode === this.MODE.EDIT) {
 
-				if (schemaFragment.type === 'array') {
+				if (schemaFragment.type.toLowerCase() === 'array') {
 					field = angular.element('<div xpsui-array-control-edit></div>');
 					field.attr('xpsui-schema', schemaPath);
 				} else 
@@ -63,7 +63,7 @@
 					field.attr('style', (width ? 'width:'+ width+'px !important;':'')
 						+ (height ? 'height:'+height+'px !important;':'')
 					);
-				} else if(schemaFragment.type === "date" 
+				} else if(schemaFragment.type.toLowerCase() === "date" 
 					|| (schemaFragment.render && schemaFragment.render.component === 'psui-datepicker')){
 					field = angular.element('<div xpsui-date-edit xpsui-calendar ></div>'); 
 					field.attr('xpsui-schema', schemaPath);
@@ -96,7 +96,7 @@
 
 			} else {
 
-				if (schemaFragment.type === 'array') {
+				if (schemaFragment.type.toLowerCase() === 'array') {
 					field = angular.element('<div xpsui-array-control-view></div>');
 					field.attr('xpsui-schema', schemaPath);
 				} else 
@@ -113,7 +113,7 @@
 
 					field.attr('width', width);
 					field.attr('height', height);
-				} else if(schemaFragment.type === "date" 
+				} else if(schemaFragment.type.toLowerCase() === "date" 
 					|| (schemaFragment.render && schemaFragment.render.component === 'psui-datepicker')){
 					field = angular.element('<div xpsui-date-view></div>');
 				// } else if( schemaFragment.type === "string"
@@ -193,11 +193,11 @@
 								component.attr('xpsui-options', localSchemaPath);
 								component.attr('xpsui-model', localModelPath);
 								component.attr('xpsui-fieldset', mode);
-							} else if (localFragment.type === 'string' 
-								|| localFragment.type === 'date' 
-								|| localFragment.type === 'array'
-								|| localFragment.type === 'number'
-								|| localFragment.type === 'object'
+							} else if (localFragment.type.toLowerCase() === 'string' 
+								|| localFragment.type.toLowerCase() === 'date' 
+								|| localFragment.type.toLowerCase() === 'array'
+								|| localFragment.type.toLowerCase() === 'number'
+								|| localFragment.type.toLowerCase() === 'object'
 								|| localFragment.type.toLowerCase() === 'decimal'
 							) {
 								component = this.generatePropertyRow(localFragment, localSchemaPath, localModelPath, mode);
@@ -217,7 +217,7 @@
 						log.groupEnd();
 					}
 				} else {
-						log.warn('Schema fragment type %s not implemented yet', schemaFragment.type);
+						log.warn('Schema fragment type "%s" not implemented yet', schemaFragment.type);
 				}
 			} else {
 				log.warn('Schema fragment has not explicit type defined');

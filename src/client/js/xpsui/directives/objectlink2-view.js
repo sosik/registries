fbbb38c3fd39c743ca36452e80394d2095dfc318
stefan-lr <stephan.varga+1@gmail.com>
2015-05-13 11:21:58 +0200

    Clickable objectlink2 elements.
    Objectlink2: The user can now click on an icon and will be redirected
    to view of object referenced in objectlink.

--- src/client/js/xpsui/directives/objectlink2-view.js
+++ src/client/js/xpsui/directives/objectlink2-view.js
@@ -2,8 +2,8 @@
 	'use strict';
 
 	angular.module('xpsui:directives')
-	.directive('xpsuiObjectlink2View', ['xpsui:logging', 'xpsui:Objectlink2Factory','xpsui:SelectDataFactory', 'xpsui:SchemaUtil','$parse',
-	function(log, objectlink2Factory, dataFactory, schemaUtil, $parse) {
+	.directive('xpsuiObjectlink2View', ['xpsui:logging', 'xpsui:Objectlink2Factory','xpsui:SelectDataFactory', 'xpsui:SchemaUtil','$parse', '$compile', 'xpsuiuriescapeFilter',
+	function(log, objectlink2Factory, dataFactory, schemaUtil, $parse, $compile, xpsuiuriescape) {
 		return {
 			restrict: 'A',
 			require: ['ngModel'],
@@ -31,15 +31,21 @@
 							schemaFragment.objectLink2.schema, 
 							schemaFragment.objectLink2.fields, 
 							function(fields){
+								var data = dataFactory.getObjectLinkData(
+										schemaFragment.objectLink2, ngModel.$viewValue
+									);
+								var schemaUri = schemaFragment.objectLink2.schema;
+								schemaUri = schemaUri.substring(0, schemaUri.length - '/view'.length);
+								view.append('<a href="#/registry/view/' + xpsuiuriescape(schemaUri) + '/' + data.oid + '" ><i class="icon-external-link fa-1"></i></a>&nbsp;&nbsp;');
 								objectlink2Factory.renderElement(
 									view, 
 									fields, 
-									dataFactory.getObjectLinkData(
-										schemaFragment.objectLink2, ngModel.$viewValue
-									)
+									data,
+									scope
 								);
 							}
 						);
+
 					}
 				};
 			}

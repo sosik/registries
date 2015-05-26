24c121ff4793fa694b966a8fbe7d294234ba5742
stefan-lr <stephan.varga+1@gmail.com>
2015-05-26 17:27:05 +0200

    Graphical design of portal editor for CityDownhill.
    Design of portal editor for city downhill was improved to look more like
    the real portal.

--- src/client/js/xpsui/directives/portal-widget-ranking-view.js
+++ src/client/js/xpsui/directives/portal-widget-ranking-view.js
@@ -17,14 +17,16 @@
 				elm.addClass('portal-article-ranking');
 
 
-				var content = angular.element('<div style="padding-left: 2px;" class="ranking">' +
+				var content = angular.element('<div class="portal-content-title"></div>' +
+						'<div style="padding-left: 2px;" class="ranking">' +
+						'<div class="portal-content-actual-ranking">' +
 						'<div ng-repeat="result in data.data.results" class="actual-ranking-riders-items">' +
-								'<div class="rider-name">{{$index}}. {{result.name}}</div>' +
+								'<div class="rider-name">{{$index+1}}. {{result.name}}</div>' +
 								'<div class="rider-country">({{result.country}})</div>' +
 								'<div class="rider-points">({{result.points}})</div>' +
-								'<div class="rider-graph" style="background: #b62480; width: {{result.percent}}%; "></div>' +
+								'<div class="rider-graph" style=" width: {{result.percent}}%; "></div>' +
 						'</div>' +
-					'</div>');
+					'</div></div>');
 
 				elm.append(content);
 

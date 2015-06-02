961e34fe40e87ed498254581652d589bb87be1c5
stefan-lr <stephan.varga+1@gmail.com>
2015-06-02 09:27:42 +0200

    Portal editor: locations and riderszone can now slide left/right.
    CDH Locations and Riderszone could not be edited in portal. Now user
    can slide locations and riderszone left/right and edit any
    of the associated articles.

--- src/client/js/xpsui/directives/portal-widget-showcase-view.js
+++ src/client/js/xpsui/directives/portal-widget-showcase-view.js
@@ -9,13 +9,16 @@
 				data: '=xpsuiPortalWidgetShowcaseView'
 			},
 			template: '<div class="portal-content-title"></div>'
-				+ '<article ng-repeat="c in model" ng-show="visibleIndex == $index || visibleIndex == \'no\'">'
+				+ '<div class="portal-content-container" style=" margin-left: {{marginLeft}}px; "><article ng-repeat="c in model" ng-show="visibleIndex == $index || visibleIndex == \'no\'">'
 				+ '<div class="x-portal-widget-showcase-diamond only-location"><div class="x-portal-widget-showcase-diamond-inner"></div></div>'
+				+ '<div class="only-riderszone" style="background: #b9b9b9; -webkit-clip-path: polygon(0px 110px, 110px 0px, 220px 110px, 110px 220px); position: absolute; width: 220px; height: 220px; margin-left: -15px; margin-top: -15px;"></div>'
+				+ '<div class="only-riderszone" style="background: url(\'{{ c.img.img }}\'); background-size: cover; background-repeat: no-repeat; ; -webkit-clip-path: polygon(0px 95px, 95px 0px, 190px 95px, 95px 190px); width: 190px; height: 190px; "></div>'
 				+ '<div ng-show="c.img.img" class="x-portal-widget-showcase-image" style="background: url(\'{{c.img.img}}\'); background-repeat: no-repeat; background-size: cover; height: 492px; width: 100%;"></div>'
 				+ '<div ng-show="c.img.img" class="x-portal-widget-showcase-textblock"><a ng-click="navigate(c.id)" ng-bind-html="makeSafe(c.title)"></a><div ng-bind-html="makeSafe(c.abstract)"></div></div>'
 				+ '<div ng-show="c.img1170.img" style="background: url(\'{{c.img1170.img}}\'); background-repeat: no-repeat; background-size: cover; height: 570px; width: 100%;"></div>'
 				+ '<div ng-show="c.img1170.img" class="x-portal-widget-showcase-textblock"><a ng-click="navigate(c.id)" ng-bind-html="makeSafe(c.title)"></a></div>'
-				+ '</article>',
+				+ '</article></div>'
+				+ '<div class="portal-content-navigation"><i class="fa fa-caret-left fa-2x" ng-click="moveLeft()"></i> <i class="fa fa-caret-right fa-2x" ng-click="moveRight()"></i></div>',
 			link: function(scope, elm, attrs, ctrls) {
 				log.group('portal-widget-category-view Link');
 
@@ -24,10 +27,18 @@
 				scope.model = [];
 
 				scope.visibleIndex = 0;
+				scope.marginLeft = 0;
 
+				scope.moveLeft = function() {
+					scope.marginLeft = scope.marginLeft + 100;
+				}
+				scope.moveRight = function() {
+					scope.marginLeft = scope.marginLeft - 100;
+				}
 				elm.css('height', '492px');
 
-				if (attrs.class.indexOf('portal-content-location') < 0) {
+				if (attrs.class.indexOf('portal-content-location') < 0
+						&& attrs.class.indexOf('portal-content-riderszone') < 0) {
 					$interval(function() {
 						++scope.visibleIndex;
 						if (scope.visibleIndex >= scope.model.length) {

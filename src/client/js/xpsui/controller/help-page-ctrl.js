dc6a5911ac8fdf0c2e86a73370bd8c9a13d9bac0
Pavol Starek <pavol@starek.sk>
2015-06-22 11:47:00 +0200

    Help page introduced
    Infrastructure for dataset specific help. There should be own help
    page in dataset partials.
    Raised compatibility version.

---
+++ src/client/js/xpsui/controller/help-page-ctrl.js
@@ -0,0 +1,12 @@
+(function(angular) {
+	'use strict';
+
+	angular.module('xpsui:controllers')
+	.controller('xpsui:HelpPageCtrl', [
+		'$scope',
+		function($scope) {
+			$scope.greeting = 'Help';
+			console.log($scope.greeting);
+		}
+	]);
+}(window.angular));
